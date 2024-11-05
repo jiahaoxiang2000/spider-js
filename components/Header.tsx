e5ffec8d285f5b9700f05090ca66d94a686b15d0
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-11-05 13:04:13 +0800

    change head: not the head title

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header>
-      <h1>My Application</h1>
       <nav>
         <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
           <li style={{ marginRight: '1rem' }}><a href="/">Home</a></li>
