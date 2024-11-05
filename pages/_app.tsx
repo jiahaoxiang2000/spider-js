dad4238043def3d9c6f424caa52bbb973efc80a4
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-11-05 13:34:49 +0800

    default dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,9 @@ import Header from '../components/Header';
 import 'tailwindcss/tailwind.css';
 
 const MyApp = ({ Component, pageProps }) => {
-  const [theme, setTheme] = useState('light');
-
-  useEffect(() => {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      setTheme('dark');
-    }
-  }, []);
 
   return (
-    <div className={`${theme} min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100`}>
+    <div className={`dark min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100`}>
       <Header />
 
       <Component {...pageProps} />
